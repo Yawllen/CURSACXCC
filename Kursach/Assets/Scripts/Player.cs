1a87e84540e47055034d3efddc1f1ac26b4f7e14
Yawllen <91437231+Yawllen@users.noreply.github.com>
2022-05-06 23:19:03 +0300

    2 video

--- Kursach/Assets/Scripts/Player.cs
+++ Kursach/Assets/Scripts/Player.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
+
+    private float movementInputDirectoin;
+    private bool isFacingRight = true;
+
+
+    public float movementSpeed = 10.0f;
+    public float jumpForce = 16.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckInput();
+        CheckMovementDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+
+    private void CheckMovementDirection()
+    {
+        if(isFacingRight && movementInputDirectoin <0)
+        {
+            Flip();
+        }
+        else if(!isFacingRight && movementInputDirectoin >0 )
+        {
+            Flip();
+        }
+    }
+
+    private void CheckInput()
+    {
+        movementInputDirectoin = Input.GetAxisRaw("Horizontal");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void ApplyMovement()
+    {
+        rb.velocity = new Vector2(movementSpeed * movementInputDirectoin, rb. velocity.y);
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
     }
 }
