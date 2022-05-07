7fd318e426eed142a0d9793f264727d91d28b82c
Yawllen <91437231+Yawllen@users.noreply.github.com>
2022-05-07 23:29:31 +0300

    3

--- Kursach/Assets/Scripts/Player.cs
+++ Kursach/Assets/Scripts/Player.cs
@@ -6,18 +6,32 @@ public class Player : MonoBehaviour
 {
 
     private Rigidbody2D rb;
+    private Animator anim;
+
+    private int amountOfJumpsLeft;
 
     private float movementInputDirectoin;
     private bool isFacingRight = true;
+    private bool isWalking;
+    private bool isGrounded;
+    private bool canJump;
 
-
+    public int amountOfJumps = 1;
     public float movementSpeed = 10.0f;
     public float jumpForce = 16.0f;
+    public float groundCheckRadius;
+
+
+    public Transform groundCheck;
+
+    public LayerMask whatIsGround;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        amountOfJumpsLeft = amountOfJumps;
     }
 
     // Update is called once per frame
@@ -25,23 +39,63 @@ public class Player : MonoBehaviour
     {
         CheckInput();
         CheckMovementDirection();
+        UpdateAnimatoins();
+        CheckIfCanJump();
     }
 
     private void FixedUpdate()
     {
         ApplyMovement();
+        CheckSurroundings();
+    }
+
+    private void CheckIfCanJump()
+    {
+        if(isGrounded && rb.velocity.y <= 0)
+        {
+            amountOfJumpsLeft = amountOfJumps;
+        }
+        if(amountOfJumpsLeft <= 0)
+        {
+            canJump = false;
+        }
+        else
+        {
+            canJump = true;
+        }
+    }
+
+    private void CheckSurroundings()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
     }
 
     private void CheckMovementDirection()
     {
-        if(isFacingRight && movementInputDirectoin <0)
+        if(isFacingRight && movementInputDirectoin < 0)
         {
             Flip();
         }
-        else if(!isFacingRight && movementInputDirectoin >0 )
+        else if(!isFacingRight && movementInputDirectoin > 0 )
         {
             Flip();
         }
+
+        if (Mathf.Abs(rb.velocity.x) >= 0.01f)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
+    }
+
+    private void UpdateAnimatoins()
+    {
+        anim.SetBool("isWalking", isWalking);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetFloat("yVelocity", rb.velocity.y);
     }
 
     private void CheckInput()
@@ -56,7 +110,12 @@ public class Player : MonoBehaviour
 
     private void Jump()
     {
-        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        if (canJump)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            amountOfJumpsLeft--;
+        }
+       
     }
 
     private void ApplyMovement()
@@ -68,5 +127,10 @@ public class Player : MonoBehaviour
     {
         isFacingRight = !isFacingRight;
         transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawSphere(groundCheck.position, groundCheckRadius);
     }
 }
